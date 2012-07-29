081b6621886feeaec67d2574130d155980fbe2f9
Kwan Hong Lee <kwan@redstar.com>
2012-07-29 16:41:14 -0400

    disabled collect static and set MEDIA_URL to s3

--- winedora/settings.py
+++ winedora/settings.py
@@ -68,7 +68,7 @@ MEDIA_ROOT = PROJECT_ROOT+'/sitemedia/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://s3.amazonaws.com/temp.vinely.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -99,6 +99,7 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # Make this unique, and don't share it with anybody.
