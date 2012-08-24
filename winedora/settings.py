a51ae541ad695a1f58d862326f6b2a82573aec0f
Kwan Hong Lee <kwan@redstar.com>
2012-08-23 21:30:06 -0400

    added alternategothic fonts and disabled ssl for non-deploy

--- winedora/settings.py
+++ winedora/settings.py
@@ -141,7 +141,19 @@ TEMPLATE_LOADERS = (
 #    'django.template.loaders.eggs.Loader',
 )
 
-MIDDLEWARE_CLASSES = (
+if DEPLOY:
+  MIDDLEWARE_CLASSES = (
+      'sslify.middleware.SSLifyMiddleware',
+      'django.middleware.common.CommonMiddleware',
+      'django.contrib.sessions.middleware.SessionMiddleware',
+      'django.middleware.csrf.CsrfViewMiddleware',
+      'django.contrib.auth.middleware.AuthenticationMiddleware',
+      'django.contrib.messages.middleware.MessageMiddleware',
+      # Uncomment the next line for simple clickjacking protection:
+      # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  )
+else:
+  MIDDLEWARE_CLASSES = (
     'sslify.middleware.SSLifyMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -150,7 +162,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+  )
 
 ROOT_URLCONF = 'winedora.urls'
 
