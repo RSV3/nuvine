42163f203b018653df6de91300731192f6624536
Kwan Hong Lee <kwan@redstar.com>
2012-08-29 12:24:07 -0400

    created admin page to change user pro application

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,14 +3,38 @@ from accounts.models import UserProfile
 from django.contrib.auth.models import Group
 
 
-class VinelyProApplicants(admin.ModelAdmin):
+def approve_pro(modeladmin, request, queryset):
+  for obj in queryset:
+    user = obj.user
+    user.groups.clear()
+    pro_group = Group.objects.get(name="Vinely Pro")
+    user.groups.add(pro_group)
+    # TODO: need to send out e-mail to the user
 
-  list_display = ('email', 'full_name', 'image', 'dob', 'phone', 'news_optin', 'wine_personality', 'is_pro')
+approve_pro.short_description = "Approve selected users as Vinely Pro"
+
+def defer_pro_privileges(modeladmin, request, queryset):
+  for obj in queryset:
+    user = obj.user
+    user.groups.clear()
+    pro_group = Group.objects.get(name="Pending Vinely Pro")
+    user.groups.add(pro_group)
+
+defer_pro_privileges.short_description = "Defer selected users Vinely Pro permissions"
+
+class VinelyUserProfileAdmin(admin.ModelAdmin):
+
+  list_display = ('email', 'full_name', 'image', 'dob', 'phone', 'zipcode', 'news_optin_flag', 'wine_personality', 'is_pro')
   list_filter = ('user__groups',)
+  list_editable = ('wine_personality', )
   raw_id_fields = ('user', )
   model = UserProfile
-
-  #news_option.short_description = 'Newsletter Optin'
+  actions = [approve_pro, defer_pro_privileges]
+  
+  def news_optin_flag(self, instance):
+    return instance.news_optin
+  news_optin_flag.short_description = 'Newsletter Optin'
+  news_optin_flag.boolean = True
 
   def email(self, instance):
     return instance.user.email
@@ -25,7 +49,8 @@ class VinelyProApplicants(admin.ModelAdmin):
     pro = Group.objects.get(name="Vinely Pro")
     return pro in instance.user.groups.all()
 
-admin.site.register(UserProfile, VinelyProApplicants)
+
+admin.site.register(UserProfile, VinelyUserProfileAdmin)
 
 # need to create a place where Elizabeth can see all the Vinely Pro's
 # and need to be able to approve
