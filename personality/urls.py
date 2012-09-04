71b13640253cd938a19b2d8a8617173998ce0127
Kwan Hong Lee <kwan@redstar.com>
2012-09-04 16:40:56 -0400

    Fixed issue #73, people with existing personalities cannot be modified

--- personality/urls.py
+++ personality/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('personality.views',
     url(r'^me/$', 'my_wine_personality', name='my_wine_personality'),
+    url(r'^check/personality/exists/$', 'check_personality_exists', name='check_personality_exists'),
     url(r'^details/(?P<user_id>\d+)/(?P<order_id>[\-\w]+)/$', 'personality_details', name="personality_details"),
     url(r'^pre/questionnaire/general/$', 'pre_questionnaire_general', name='pre_questionnaire_general'),
     url(r'^pre/questionnaire/wine/$', 'pre_questionnaire_wine', name='pre_questionnaire_wine'),
