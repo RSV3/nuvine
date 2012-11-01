0bd7a2c66bf3346dc8cad440b0e1bff67fd896fc
odero <billyx5@gmail.com>
2012-11-01 17:05:54 +0300

    Fixed #294 Wine rating form doesn't show up in the beginning when the page is loaded

--- personality/views.py
+++ personality/views.py
@@ -239,7 +239,7 @@ def record_all_wine_ratings(request, email=None, party_id=None, rate=1):
   party = None
 
   party = get_object_or_404(Party, pk=party_id)
-  
+
   # used in ratings_saved.html template to go back to party details
   data["party_id"] = party_id
   data["party"] = party
@@ -486,7 +486,7 @@ def record_all_wine_ratings(request, email=None, party_id=None, rate=1):
 
     # set active tab to be next wine where 'feeling' is not filled
     ratings = taster.wineratingdata_set.exclude(overall__in=range(1, 6))
-    data['active_tab'] = ratings[0].wine.number if ratings else 0
+    data['active_tab'] = ratings[0].wine.number if ratings else 1
     data["rate_wines_menu"] = True
     data["form"] = form
 
