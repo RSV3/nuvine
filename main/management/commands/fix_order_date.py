a49c247feab6b34d0a1d6c741a2750a8876d6717
Kwan Hong Lee <kwan@redstar.com>
2012-11-30 22:03:37 -0500

    working on fixing order date for previous orders

--- main/management/commands/fix_order_date.py
+++ main/management/commands/fix_order_date.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
 
+from dateutil.relativedelta import relativedelta
 from datetime import datetime, timedelta
 from main.models import Order, PartyInvite
+from accounts.models import SubscriptionInfo
 
 class Command(BaseCommand):
   args = ""
@@ -23,7 +25,31 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
     for receiver_email in Order.objects.values_list('receiver__email', flat=True).distinct():
-      for o in Order.objects.filter(receiver__email=receiver_email):
+      subscription = SubscriptionInfo.objects.filter(user__email=receiver_email).order_by('-updated_datetime')
+      if subscription.exists():
+        subscription = subscription[0]
+        print subscription.get_frequency_display()
+      else:
+        print "No subscription"
+
+      # item.price_category == 11 then it's host tasting kit (date should be 5 days before party)
+      # item.frequency == [0, 9] no subscription
+      # item.frequency == 1  monthly
+      # item.frequency == 2  bi-monthly
+      # item.frequency == 3  quarterly
+
+      # for each user that has ordered
+      last_order_date = None
+      for o in Order.objects.filter(receiver__email=receiver_email).order_by('id'):
+        tasting_kit = False
+        frequency = 0
+
+        for item in o.cart.items.all():
+          if item.price_category == 11:
+            tasting_kit = True
+            break
+          frequency = item.frequency
+
         print o.receiver.email, 
         print o.get_fulfill_status_display(), [item for item in o.cart.items.all()]
 
@@ -33,8 +59,21 @@ class Command(BaseCommand):
           inv = invites[0]
           diff_days = o.order_date - inv.party.event_date
           if diff_days.days > 5:
-            o.order_date = inv.party.event_date + timedelta(days=1)
+            if tasting_kit:
+              o.order_date = inv.party.event_date - timedelta(days=5)
+            elif frequency == [0, 9]:
+              # it's wine subscription use frequency
+              o.order_date = inv.party.event_date + timedelta(days=1)
+              last_order_date = o.order_date
+            else:
+              if not last_order_date:
+                o.order_date = inv.party.event_date + timedelta(days=1)
+              else:
+                o.order_date = last_order_date + relativedelta(months=+frequency)
+              last_order_date = o.order_date
             o.save()
-          print "\tOrder date: %s - Party date: %s - Days diff: %s" % (o.order_date, inv.party.event_date, diff_days.days)
+
+          diff_days = o.order_date - inv.party.event_date
+          print "\tNew Order date: %s - Party date: %s - Days diff: %s" % (o.order_date, inv.party.event_date, diff_days.days)
         else:
           print "\tOrder date: %s - No parties" % o.order_date
