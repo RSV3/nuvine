ba9d77715871a0f25fd0cb5fd1db4bd59387b03f
odero <billyx5@gmail.com>
2013-01-25 17:45:27 +0300

    Charge stripe for MI, CA orders when update_orders is executed

--- accounts/management/commands/update_orders.py
+++ accounts/management/commands/update_orders.py
@@ -11,10 +11,11 @@ from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 from django.http import HttpRequest
 from django.conf import settings
-
+from django.db.models import Q
 from main.utils import send_order_confirmation_email
 
 import stripe
+
 
 class Command(BaseCommand):
 
@@ -22,8 +23,8 @@ class Command(BaseCommand):
   help = 'Daily process to check the subscription information, create new order and add next order if they are due today'
 
   def handle(self, *args, **options):
-
-    for user_id in SubscriptionInfo.objects.exclude(frequency__in=[0, 9]).filter(quantity__gt=0).values_list('user', flat=True).distinct():
+    # Get all users whose subscriptions are not already being handled by stripe
+    for user_id in SubscriptionInfo.objects.exclude(Q(frequency__in=[0, 9]) | Q(quantity=0) | Q(user__userprofile__stripe_card__isnull=False)).values_list('user', flat=True).distinct():
       user = User.objects.get(id=user_id)
 
       # need to work with only the latest subscription info per user
@@ -39,20 +40,34 @@ class Command(BaseCommand):
           # full case
           quantity_code = 1
           if subscription.quantity == 5:
-            product = Product.objects.get(id=4)
+            # product = Product.objects.get(id=4)
+            product = Product.objects.get(cart_tag='basic')
           elif subscription.quantity == 7:
-            product = Product.objects.get(id=5)
+            # product = Product.objects.get(id=5)
+            product = Product.objects.get(cart_tag='divine')
           elif subscription.quantity == 9:
-            product = Product.objects.get(id=6)
+            # product = Product.objects.get(id=6)
+            product = Product.objects.get(cart_tag='superior')
         elif subscription.quantity in [6, 8, 10]:
           # half case
           quantity_code = 2
           if subscription.quantity == 6:
-            product = Product.objects.get(id=4)
+            # product = Product.objects.get(id=4)
+            product = Product.objects.get(cart_tag='basic')
           elif subscription.quantity == 8:
-            product = Product.objects.get(id=5)
+            # product = Product.objects.get(id=5)
+            product = Product.objects.get(cart_tag='divine')
           elif subscription.quantity == 10:
-            product = Product.objects.get(id=6)
+            # product = Product.objects.get(id=6)
+            product = Product.objects.get(cart_tag='superior')
+        elif subscription.quantity in [12, 13, 14]:
+          quantity_code = 3
+          if subscription.quantity == 12:
+            product = Product.objects.get(cart_tag='3')
+          elif subscription.quantity == 13:
+            product = Product.objects.get(cart_tag='6')
+          elif subscription.quantity == 14:
+            product = Product.objects.get(cart_tag='12')
 
         if product is None:
           print "%s %s <%s> does not have a subscription product" % (user.first_name, user.last_name, user.email)
@@ -89,6 +104,7 @@ class Command(BaseCommand):
 
         prof = user.get_profile()
         shipping_address = prof.shipping_address
+
         card = prof.credit_card
         today = datetime.now(tz=UTC())
         order = Order(ordered_by=user, receiver=user, cart=cart,
@@ -97,46 +113,54 @@ class Command(BaseCommand):
         order.save()
         print "Created a new order for %s %s <%s>" % (user.first_name, user.last_name, user.email)
 
-        """
-        # TODO: need to charge credit card if necessary
-        receiver_state = Zipcode.objects.get(code=shipping_address.zipcode).state
+        # determine if need to use stripe or native processing
+        receiver_state = shipping_address.state
 
-        if receiver_state == "MI":
-          stripe.api_key = settings.STRIPE_SECRET
-          stripe_card = prof.stripe_card
+        if receiver_state in Cart.STRIPE_STATES:
+          if receiver_state == 'MI':
+            stripe.api_key = settings.STRIPE_SECRET
+          elif receiver_state == 'CA':
+            stripe.api_key = settings.STRIPE_SECRET_CA
+          credit_card = prof.credit_card
+          print 'card_number', credit_card.decrypt_card_num(), 'cvv', credit_card.decrypt_cvv()
+          if settings.DEPLOY:
+            card_number = credit_card.decrypt_card_num()
+            cvc = credit_card.decrypt_cvv()
+          else:
+            cvc = '111'
+            if credit_card.card_type == 'American Express':
+              card_number = '378282246310005'
+            elif credit_card.card_type == 'Master Card':
+              card_number = '5105105105105100'
+            else:
+              card_number = '4242424242424242'
 
+          card = {'number': card_number, 'exp_month': credit_card.exp_month, 'exp_year': credit_card.exp_year,
+                  'name': '%s %s' % (user.first_name, user.last_name), 'address_zip': credit_card.billing_zipcode,
+                  }
+
+          # some cards dont have a verification code so only include cvc for those that have
+          if cvc:
+            card['cvc'] = cvc
+
+          # no record of this customer-card mapping so create
           try:
-            customer = stripe.Customer.retrieve(id=stripe_card.stripe_user)
-            if customer.get('deleted'):
-              raise Exception('Customer Deleted')
-            stripe_user_id = customer.id
+            customer = stripe.Customer.create(card=card, email=user.email)
+            # up profile
+            stripe_card = StripeCard.objects.create(stripe_user=customer.id, exp_month=customer.active_card.exp_month,
+                                exp_year=customer.active_card.exp_year, last_four=customer.active_card.last4,
+                                card_type=customer.active_card.type, billing_zipcode=credit_card.billing_zipcode)
 
-            # makes sure we have the exact same card
-            stripe_card = StripeCard.objects.get(stripe_user=stripe_user_id, exp_month=request.POST.get('exp_month'),
-                              exp_year=request.POST.get('exp_year'), last_four=request.POST.get('last4'),
-                              card_type=request.POST.get('card_type'), billing_zipcode=request.POST.get('address_zip'))
-          except:
-            print "Stripe account does not work for:", user.email
-            continue
+            prof.stripe_card = stripe_card
+            prof.save()
+            prof.stripe_cards.add(stripe_card)
 
-          stripe.api_key = settings.STRIPE_SECRET
-          # NOTE: Amount must be in cents
-          # Having these first so that they come last in the stripe invoice.
-          stripe.InvoiceItem.create(customer=prof.stripe_card.stripe_user, amount=int(order.cart.shipping() * 100), currency='usd', description='Shipping')
-          stripe.InvoiceItem.create(customer=prof.stripe_card.stripe_user, amount=int(order.cart.tax() * 100), currency='usd', description='Tax')
-          non_sub_orders = order.cart.items.filter(frequency=0)
-          for item in non_sub_orders:
-            # one-time only charged immediately at this point
-            stripe.InvoiceItem.create(customer=prof.stripe_card.stripe_user, amount=int(item.subtotal() * 100), currency='usd', description=LineItem.PRICE_TYPE[item.price_category][1])
-
-          # if subscription exists then create plan
-          sub_orders = order.cart.items.filter(frequency__in=[1, 2, 3])
-          if sub_orders.exists():
-            item = sub_orders[0]
-            customer = stripe.Customer.retrieve(id=prof.stripe_card.stripe_user)
+            stripe.InvoiceItem.create(customer=customer.id, amount=int(order.cart.tax() * 100), currency='usd', description='Tax')
             stripe_plan = SubscriptionInfo.STRIPE_PLAN[item.frequency][item.price_category - 5]
             customer.update_subscription(plan=stripe_plan)
-        """
+          except Exception, e:
+            print "Error processing card with stripe", e
+            print 'Card was declined by stripe for %s %s <%s>' % (user.first_name, user.last_name, user.email)
 
         # need to update next invoice date on subscription
         if subscription.frequency == 1:
