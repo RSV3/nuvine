46e5c0b419609dc39b84b80801f605a5d043d38f
odero <billyx5@gmail.com>
2013-05-07 12:48:39 +0300

    Only send welcome email the first time user gets personality from rating

--- personality/views.py
+++ personality/views.py
@@ -515,7 +515,8 @@ def record_all_wine_ratings(request, email=None, party_id=None, rate=1):
             verification_code = str(uuid.uuid4())
             vque = VerificationQueue(user=taster, verification_code=verification_code)
             vque.save()
-          send_welcome_to_vinely_email(request, taster, verification_code, temp_password)
+            # only send email for new users
+            send_welcome_to_vinely_email(request, taster, verification_code, temp_password)
         return render_to_response("personality/ratings_saved.html", data, context_instance=RequestContext(request))
       else:
         msg = "Partial ratings have been saved for %s. <a href='%s'>Enter ratings for next taster.</a>" % (taster.email, reverse('party_details', args=[party.id]))
