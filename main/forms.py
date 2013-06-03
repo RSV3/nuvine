563dbe031f73d41b7ed90fbdfd4b21c0c7d4eca2
odero <billyx5@gmail.com>
2013-06-03 21:24:11 +0300

    Fixed party creation bug requiring fee at creation

--- main/forms.py
+++ main/forms.py
@@ -96,7 +96,7 @@ class PartyCreateForm(forms.ModelForm):
 
   class Meta:
     model = Party
-    exclude = ['setup_stage']
+    exclude = ['setup_stage', 'fee']
 
   def __init__(self, *args, **kwargs):
     user = kwargs.pop('user')
