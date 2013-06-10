aaa7433ddb83137944b73132897eefbc7485c1c8
odero <billyx5@gmail.com>
2013-06-10 23:48:07 +0300

    Need to use the patched django-tastypie-swagger

--- main/api/tools.py
+++ main/api/tools.py
@@ -1,10 +1,14 @@
-from tastytools.api import Api
+from tastypie.api import Api
 from main.api.resources import UserResource, UserProfileResource, \
                                 PartyResource, PartyInviteResource, \
                                 WinePersonalityResource, WineRatingDataResource
 # from main.api.resources import AnoterResource, YetAnotherResource
 
 api = Api()
-api.register(resources=[UserResource, UserProfileResource])
-api.register(resources=[PartyResource, PartyInviteResource])
-api.register(resources=[WineRatingDataResource, WinePersonalityResource])
+
+resources = [UserResource, UserProfileResource]
+resources += [PartyResource, PartyInviteResource]
+resources += [WineRatingDataResource, WinePersonalityResource]
+
+for r in resources:
+  api.register(r())
